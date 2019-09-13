fd8f415be5483d222dfa75c15970d5a935184a58
spiritxhx <spiritxhx@gmail.com>
2019-09-13 16:46:36 -0400

    Updates

---
+++ precache-manifest.d2f445a59d80a958c11aa763551713b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c8f3e350fa26613ea71b54638212d8d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b37b1b0c3c29c57e7c62",
+    "url": "/static/css/main.e8338068.chunk.css"
+  },
+  {
+    "revision": "46d6f54636a594326caa",
+    "url": "/static/js/2.3ad1167e.chunk.js"
+  },
+  {
+    "revision": "b37b1b0c3c29c57e7c62",
+    "url": "/static/js/main.097d81c6.chunk.js"
+  },
+  {
+    "revision": "47328694963902aade01",
+    "url": "/static/js/runtime~main.37b2fca3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
