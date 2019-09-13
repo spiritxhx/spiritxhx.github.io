ee476f2ba1ce1551ead7c354d6c9092686f811bf
spiritxhx <spiritxhx@gmail.com>
2019-09-13 17:19:05 -0400

    create the navbar component

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 import './App.css';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
+import Navbar from './component/Navbar';
+
 const App: React.FC = () => {
   return (
-    
+    <React.Fragment>
+      <Navbar />
       <Router>
 
       </Router>
+    </React.Fragment>
   );
 }
 
---
+++ src/component/Navbar.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <div className="navbar navbar-expand-lg navbar-expand-md navbar-light bg-light fixed-top">
+      Hello from Navbar
+    </div>
+  )
+}
