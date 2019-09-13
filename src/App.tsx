29f52961577a9017d205dd891be3b6f7f642f965
spiritxhx <spiritxhx@gmail.com>
2019-09-13 16:59:12 -0400

    add react router to the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 const App: React.FC = () => {
   return (
-    <React.Fragment>
-      Hello world
-    </React.Fragment>
+    
+      <Router>
+
+      </Router>
   );
 }
 
