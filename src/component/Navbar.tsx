e0910319c9eb3f4d271cc0d48b4796fe30485ee3
spiritxhx <spiritxhx@gmail.com>
2019-09-13 17:33:06 -0400

    add react bootstrap and jquery

--- src/component/Navbar.tsx
+++ src/component/Navbar.tsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
 
 export default function Navbar() {
   return (
-    <div className="navbar navbar-expand-lg navbar-expand-md navbar-light bg-light fixed-top">
-      Hello from Navbar
+    <div className="navbar navbar-expand-lg navbar-expand-md navbar-light bg-light fixed-top justify-content-between">
+      <a className="navbar-brand" href="/">
+        Hans(Hanxiang) Xu
+      </a>
+
     </div>
-  )
+  );
 }
