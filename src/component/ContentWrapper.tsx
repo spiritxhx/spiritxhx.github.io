8ddbd8f1e3204741060dd1eb9621fe54928165f5
spiritxhx <spiritxhx@gmail.com>
2019-09-13 22:22:55 -0400

    fix the navbar toggle effect

---
+++ src/component/ContentWrapper.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function ContentWrapper() {
+  return (
+    <React.Fragment>
+      <div className="wrapper">
+        <nav>Hello from side bar!</nav>
+      </div>
+    </React.Fragment>
+  );
+}
--- src/component/NavComponent.tsx
+++ src/component/NavComponent.tsx
@@ -10,22 +10,21 @@ export default function NavComponent() {
     //   </a>
 
     // </div>
-    <Navbar
-      bg="light"
-      expand="lg"
-      fixed="top"
-      className="justify-content-between big-nav"
-    >
+    <Navbar bg="light" expand="lg" fixed="top" className="big-nav">
       <Navbar.Brand href="/">Hans(Hanxiang) Xu</Navbar.Brand>
-      <div className="d-flex">
-        <Navbar.Collapse id="collapse-nav">
+      <Navbar.Toggle aria-controls="collapse-nav" />
+      <Navbar.Collapse id="collapse-nav" className="justify-content-end">
+        <Nav>
           <Nav.Link href="/">Home</Nav.Link>
-          <Nav.Link href="https://resume.creddle.io/resume/ao5zzbuepvb" target="_blank">
+          <Nav.Link
+            href="https://resume.creddle.io/resume/ao5zzbuepvb"
+            target="_blank"
+          >
             Resume
           </Nav.Link>
           <Nav.Link href="/">Project</Nav.Link>
-        </Navbar.Collapse>
-      </div>
+        </Nav>
+      </Navbar.Collapse>
     </Navbar>
   );
 }
