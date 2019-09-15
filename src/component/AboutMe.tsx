a8218b9b823588fef3df1adb22e8f6c6fd2eaa01
spiritxhx <spiritxhx@gmail.com>
2019-09-15 18:45:22 -0400

    change container to container-fluid to fully expand

--- src/component/AboutMe.tsx
+++ src/component/AboutMe.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function AboutMe() {
   return (
     <React.Fragment>
-      <div className="container aboutme">
+      <div className="container-fluid aboutme">
         <h2 className="text-left text-capitalize pt-4">About me</h2>
         <h1>test</h1>
         <h1>test</h1>
