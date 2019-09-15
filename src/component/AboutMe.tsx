623bd710d62d68074040fd23de8c65d83af5f2e1
spiritxhx <spiritxhx@gmail.com>
2019-09-15 16:24:39 -0400

    create the aboutme component

---
+++ src/component/AboutMe.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function AboutMe() {
+  return (
+    <React.Fragment>
+      <h1 className="text-left text-capitalize">
+        About me
+      </h1>
+    </React.Fragment>
+  )
+}
