818ad8e06417248bd29db909f4d19cf8b5e11e13
spiritxhx <spiritxhx@gmail.com>
2019-09-15 16:17:29 -0400

    add background linear gradient effect

--- src/component/ContentWrapper.tsx
+++ src/component/ContentWrapper.tsx
@@ -5,7 +5,14 @@ export default function ContentWrapper() {
   return (
     <React.Fragment>
       <div className="wrapper d-flex">
-        <nav id="sidebar">Hello from side bar!</nav>
+        <nav id="sidebar">
+          <div className="text-center mt-5">
+            Hi I'm Hans
+          </div>
+          <div className="text-center mt-3">
+            A Junior Full Stack Developer
+          </div>
+        </nav>
       </div>
     </React.Fragment>
   );
