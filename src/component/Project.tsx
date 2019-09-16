06d382affbc93850d7111832df725cbc5b5c2697
spiritxhx <spiritxhx@gmail.com>
2019-09-15 22:47:48 -0400

    display the project name in the projectList

--- src/component/Project.tsx
+++ src/component/Project.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
+import {Button} from "./Button";
 
 interface projectInfo{
+  id: number;
   name: string;
   description: string;
   gitUrl: string;
@@ -10,8 +12,8 @@ interface projectInfo{
 
 export default function Project(props: projectInfo) {
   return (
-    <div>
-      
+    <div className="col-9 mx-auto col-md-6 col-lg-6 my-3">
+      {props.name}
     </div>
   )
 }
--- src/component/ProjectList.tsx
+++ src/component/ProjectList.tsx
@@ -1,10 +1,25 @@
-import React from 'react';
-import {data} from "../data/mockData";
+import React from "react";
+import { data } from "../data/mockData";
+import Project from "./Project";
+
+interface projectInfo {
+  id: number;
+  name: string;
+  description: string;
+  gitUrl: string;
+  liveUrl: string;
+  screenshot: string;
+}
 
 export default function ProjectList() {
   return (
     <React.Fragment>
       <h2>Projects</h2>
+      <div className="row">
+        {data.map((project: projectInfo, index) => {
+          return <Project {...project} key={index} />;
+        })}
+      </div>
     </React.Fragment>
-  )
+  );
 }
