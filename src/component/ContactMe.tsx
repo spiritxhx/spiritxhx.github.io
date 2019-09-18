dd6bcc41d934877bd284dc2b4632a76405fd8bee
spiritxhx <spiritxhx@gmail.com>
2019-09-18 17:08:58 -0400

    add responsive design for input form and add phone number

--- src/component/ContactMe.tsx
+++ src/component/ContactMe.tsx
@@ -4,25 +4,29 @@ import { Form, Col } from "react-bootstrap";
 export default function ContactMe() {
   return (
     <React.Fragment>
-      <h3>Contact me</h3>
+      <h2>Contact me</h2>
       <Form>
         <Form.Row>
-          <Form.Group as={Col} controlId="formFirstName">
+          <Form.Group as={Col} md="6" controlId="formFirstName">
             <Form.Label>First Name</Form.Label>
             <Form.Control
               type="firstname"
               placeholder="Enter your first name"
             />
           </Form.Group>
-          <Form.Group as={Col} controlId="formLastName">
+          <Form.Group as={Col} md="6" controlId="formLastName">
             <Form.Label>Last Name</Form.Label>
             <Form.Control type="lastname" placeholder="Enter your last name" />
           </Form.Group>
         </Form.Row>
         <Form.Row>
-          <Form.Group as={Col} controlId="formEmail">
+          <Form.Group as={Col} md="6" controlId="formEmail">
             <Form.Label>Email</Form.Label>
             <Form.Control type="email" placeholder="Enter your email" />
+          </Form.Group>
+          <Form.Group as={Col} md="6" controlId="formEmail">
+            <Form.Label>Phone Number</Form.Label>
+            <Form.Control type="phonenumber" placeholder="Enter your phone number" />
           </Form.Group>
         </Form.Row>
       </Form>
