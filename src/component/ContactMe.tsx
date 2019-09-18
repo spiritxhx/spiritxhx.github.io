20951d6bee025cc177702cc66241200b3150309d
spiritxhx <spiritxhx@gmail.com>
2019-09-18 16:47:19 -0400

    add title and lastname form to contact me

--- src/component/ContactMe.tsx
+++ src/component/ContactMe.tsx
@@ -3,13 +3,23 @@ import { Form, Col } from "react-bootstrap";
 
 export default function ContactMe() {
   return (
-    <Form>
-      <Form.Row>
-        <Form.Group as={Col} controlId="formFirstName">
-          <Form.Label>First Name</Form.Label>
-          <Form.Control type="firstname" placeholder="Enter your first name" />
-        </Form.Group>
-      </Form.Row>
-    </Form>
+    <React.Fragment>
+      <h3>Contact me</h3>
+      <Form>
+        <Form.Row>
+          <Form.Group as={Col} controlId="formFirstName">
+            <Form.Label>First Name</Form.Label>
+            <Form.Control
+              type="firstname"
+              placeholder="Enter your first name"
+            />
+          </Form.Group>
+          <Form.Group as={Col} controlId="formLastName">
+            <Form.Label>Last Name</Form.Label>
+            <Form.Control type="lastname" placeholder="Enter your last name" />
+          </Form.Group>
+        </Form.Row>
+      </Form>
+    </React.Fragment>
   );
 }
